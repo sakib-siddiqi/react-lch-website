462bded87cc0bcffba08df4c7c0638d76bf90c9e
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-10-04 22:48:41 +0600

    about page modified

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
           <div className="col-12 col-sm-12 col-md-6 text-box-center">
             <p className="text-dark">All you need to know about !</p>
             <h1 className="text-primary fst-italic fw-bold">LCH</h1>
-            <p className="text-dr-secondary w-75 text-justify p-3 bg-lt-secondary text-primary rounded-2 mb-4 lh">
+            <p className="text-dr-secondary text-justify p-3 bg-lt-secondary text-primary rounded-2 mb-4 lh">
               We are a developer team . We teach programming at low cost. We
               have more thene 2000k students.
               <p>
